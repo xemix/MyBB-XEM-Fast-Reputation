5bd27f02b36752c85545036d841e68f9701bdb0b
Genesis <tofupedia.fr@gmail.com>
2015-03-23 16:41:58 +0100

    Added french language, templates are updated automagically, updated README

---
+++ inc/languages/french/xem_fast_rep.lang.php
@@ -0,0 +1,11 @@
+<?php
+//XEM Fast Reputation English language file. by Szczepan 'Xemix' Machaj (c) 2015 [xemix.eu]
+
+$l['xem_fast_rep_like_it'] = 'J\'aime !';
+$l['xem_fast_rep_unlike_it'] = 'Je n\'aime pas !';
+$l['xem_fast_rep_who_like_it'] = 'Utilisateurs qui aiment';
+$l['you'] = 'Vous';
+$l['you_like_it'] = 'Vous aimez';
+$l['like_it'] = 'aiment';
+$l['and'] = 'and';
+$l['others_person_like_it'] = 'autres utilisateurs aiment ça';
