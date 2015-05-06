4d00c5ad464e291f3d1b6995abc72a285aaff117
ArnoldLayne-xXx <dthiago@users.noreply.github.com>
2015-05-06 02:31:35 -0300

    Create xem_fast_rep.lang.php
    Brazilian Language - Portuguese

---
+++ inc/languages/ptbr/admin/xem_fast_rep.lang.php
@@ -0,0 +1,8 @@
+<?php
+//XEM Fast Reputation Língua Brasileira MyBB User dthiago from BFBRASIL.COM.BR 06/05/2015 02H:28min
+
+$l['xem_fast_rep_description'] = 'Esta extensão permite fácil e rápidamente avaliar mensagens (+ / - ) usando o sistema de reputação do MyBB - módulo conhecido no IPB.';
+$l['xem_fast_rep_settings_title'] = 'Configurações para XEM Reputação Rápida';
+$l['xem_fast_rep_plugin_active'] = 'XEM Reputação Rápida Ativa?';
+$l['xem_fast_rep_show_liked_this'] = 'Mostrar quem curtiu as mensagens?';
+$l['xem_fast_rep_allow_unlike_posts'] = 'Permitir mudanças no status das mensagens';
